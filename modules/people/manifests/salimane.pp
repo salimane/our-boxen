c795ceab57ce04cab0792a457399fdb39a20aa4e
Salimane Adjao Moustapha <me@salimane.com>
2013-06-18 23:40:39 +0800

    add more needed modules

--- modules/people/manifests/salimane.pp
+++ modules/people/manifests/salimane.pp
@@ -1,5 +1,5 @@
 class people::salimane {
-  include zsh   # requires emacs module in Puppetfile
+  include zsh   # requires zsh  module in Puppetfile
   include firefox::beta
   include virtualbox
   include java
@@ -12,7 +12,46 @@ class people::salimane {
     source => 'sergeche/emmet-sublime'
   }
   include python
+  include appcleaner
+  include transmission
+  include xpdf
+  include wget
+  #sysctl::set { 'somekey':
+    #value => 'somevalue'
+  #}
+  include sysctl::ipforwarding
+  include screen
+  include graphviz
+#  include sourcetree
+  include dropbox
+  include vagrant
+  include spotify
+  include vlc
+  include github_for_mac
+  include imageoptim
+  include clipmenu
+  include tmux
+  include magican
+  include ccleaner
+  include adium
+  include google_notifier
+  include secondbar
+  include skype
+
+  $ruby_version = '2.0.0'
+  class { 'ruby::global':
+    version => $ruby_version
+  }
   
+  ruby::gem { "puppet for ${version}":
+    gem     => 'puppet',
+    ruby    => $ruby_version,
+  }
+
+  ruby::gem { "librarian-puppet for ${version}":
+    gem     => 'librarian-puppet',
+    ruby    => $ruby_version,
+  } 
   
   $home     = "/Users/${::luser}"
   $src       = "${home}/src"
