7732e008afda001882c314db6045870202a97508
Salimane Adjao Moustapha <salimane.moustapha@meltwater.com>
2015-07-02 23:06:05 +0200

    cleanup personal manifest

--- modules/people/manifests/salimane.pp
+++ modules/people/manifests/salimane.pp
@@ -17,35 +17,18 @@ class people::salimane {
     'quotes', 'save-session', 'sassbeautify', 'sort-lines', 'toggle-quotes'
     ]:
   }
-  # include btsync # http://labs.bittorrent.com/experiments/sync.html
-  # include chrome::beta
-  # include dockutil
-  # include firefox::beta
-  # include foreman
+
   include go
   class { 'go::global' :
     version => '1.4'
   }
-  # include googledrive
-  # include keepassx # http://www.keepassx.org/
-  # include moreutils # http://joeyh.name/code/moreutils/
   osx::recovery_message { 'If this Mac is found, please call +4915732675528': }
-  # include osx::dock::2d
-  # include osx::dock::hide_indicator_lights
-  # include osx::finder::show_all_on_desktop
   class { 'osx::global::natural_mouse_scrolling' : enabled => false }
   class { 'osx::dock::pin_position' : position => 'middle'}
   class {'osx::dock::icon_size' : size => 36}
   class {'osx::dock::position' : position => 'right'}
 
-  # include osxfuse # http://osxfuse.github.io/
-  # include parallels
-  # include propane # http://propaneapp.com/
   ensure_resource('ruby::version', '2.2.2') # install a ruby version
-  # include sysctl::ipforwarding
-  # include tmux # http://tmux.sourceforge.net/
-  # include tunnelblick # http://code.google.com/p/tunnelblick/
-  # include zsh # Installs zsh and makes it your default shell. For justice.
 
   $dotfiles = "${boxen::config::srcdir}/dotfiles"
   repository { $dotfiles:
